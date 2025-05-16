8f1ed6a7f16d0e715d27446c4877bd200edceb4c
luc0 <lucianoperezt@gmail.com>
2025-05-16 14:49:37 -0300

    analyze-rentals can be setled as debugable in .env

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,13 @@ use Illuminate\Support\Facades\Schedule;
 //Schedule::call(new AnalyzeRentals())->weekdays()
 //    ->at('19:00');
 
-Schedule::command( AnalyzeRentals::class)
-//    ->weekdays()->at('4:26')
-    ->everyThreeMinutes();
+$analyzeRentalsDebugMode = env('ANALYZE_RENTALS_DEBUG_MODE', 'true');
+
+if ($analyzeRentalsDebugMode) {
+    Schedule::command( AnalyzeRentals::class)
+        ->everyThreeMinutes();
 //    ->sendOutputTo(storage_path('logs/analyze.log'));
+} else {
+    Schedule::command( AnalyzeRentals::class)
+        ->weekdays()->at('17:00');
+}
